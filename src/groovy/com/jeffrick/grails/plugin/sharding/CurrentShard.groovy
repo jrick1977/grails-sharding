8dc0e3bbb3390efcdd427847fd2ef40de84d95bc
rwoollam <rwoollam@gmail.com>
2013-03-05 10:17:49 -0500

    Update CurrentShard.groovy
    Fixed issue with _dataSourceLookup.   ThreadLocal was a clearing out the map, this was inadvertent and causing the getIndexDatabaseURL call to fail.

--- src/groovy/com/jeffrick/grails/plugin/sharding/CurrentShard.groovy
+++ src/groovy/com/jeffrick/grails/plugin/sharding/CurrentShard.groovy
@@ -1,56 +1,64 @@
 package com.jeffrick.grails.plugin.sharding
 
+import com.jeffrick.grails.plugin.sharding.annotation.Shard
 import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
 
-import com.jeffrick.grails.plugin.sharding.annotation.Shard as ShardAnnotation
-import com.jeffrick.grails.plugins.sharding.Shard
-
 /**
- * Holds the current shard we are pointing, values are stored as thread locals.
+ * This class holds the current shard we are pointing, values are stored as thread locals
  * @author <a href='mailto:jeffrick@gmail.com'>Jeff Rick</a>
  */
 class CurrentShard {
 
-    private static final ThreadLocal<Integer> _shardIndex = new ThreadLocal<Integer>()
-    private static final ThreadLocal<Boolean> _autoCommit = new ThreadLocal<Boolean>()
-    private static final ThreadLocal<String> _indexDataSource = new ThreadLocal<String>()
-    private static final ThreadLocal<Map<String, ConfigObject>> _dataSourceLookup = new ThreadLocal<Map<String,ConfigObject>>()
+    private static final ThreadLocal _shardIndex
+    private static final ThreadLocal _autoCommit
+    private static final ThreadLocal _indexDataSource
+    private static HashMap<String, ConfigObject> _dataSourceLookup
+
+    static {
+        _shardIndex = new ThreadLocal();
+        _autoCommit = new ThreadLocal();
+        _indexDataSource = new ThreadLocal();
+        _dataSourceLookup = new HashMap<String,ConfigObject>()
+    }
 
     static ShardConfig get() {
         Integer currentId = getIndex()
-        for (shard in Shards) {
-            if (shard.id == currentId) {
-                return shard
+        ShardConfig current = null;
+        Shards.getShards().each {
+            if (it.id == currentId) {
+                current = it
             }
         }
+
+        return (current);
     }
 
-    /**
+    /** *
      * Gets the index of the current shard
      */
     static Integer getIndex() {
         if (_shardIndex.get() == null) {
-            _shardIndex.set(1)
+            _shardIndex.set(1);
         }
-        return (_shardIndex.get())
+        return ((long) _shardIndex.get())
     }
 
-    /**
+    /** *
      * Gets whether the current shard is set to Auto Commit
      */
     static boolean getAutoCommit() {
         if (_autoCommit.get() == null) {
             return true
         } else {
-            return (_autoCommit.get())
+            return ((boolean) _autoCommit.get())
         }
     }
 
-    /**
+    /** *
      * Sets whether the current shard is set to Auto Commit
      */
     static void setAutoCommit(boolean autoCommit) {
-        _autoCommit.set autoCommit
+        _autoCommit.set autoCommit;
     }
 
     /** *
@@ -61,25 +69,26 @@ class CurrentShard {
         _shardIndex.set(shard.id)
     }
 
-    static void setDataSourceLookup(Map<String, ConfigObject> dataSourceLookup) {
-        _dataSourceLookup.set(dataSourceLookup)
+    static void setDataSourceLookup(HashMap<String, ConfigObject> dataSourceLookup) {
+        _dataSourceLookup = dataSourceLookup
     }
 
     static String getIndexDatabaseURL() {
+        println "getIndexDatabaseURL"
         String dataSourceName = getIndexDataSourceName()
-        Map<String, ConfigObject> lookup = _dataSourceLookup.get()
-        return(lookup.get(dataSourceName).getProperty("url"))
+        println "dataSourceName = $dataSourceName"
+        return(_dataSourceLookup.get(dataSourceName).getProperty("url"))
     }
 
-    static String getIndexDataSourceName() {
+    static public String getIndexDataSourceName() {
         if (_indexDataSource.get() == null) {
 
-            def grailsApplication = new Shard().domainClass.grailsApplication
+            def grailsApplication = new com.jeffrick.grails.plugins.sharding.Shard().domainClass.grailsApplication
 
             grailsApplication.domainClasses.each {
                 DefaultGrailsDomainClass domainClass ->
-                    if (domainClass.clazz.isAnnotationPresent(ShardAnnotation)) {
-                        ShardAnnotation prop = domainClass.clazz.getAnnotation(ShardAnnotation)
+                    if (domainClass.clazz.isAnnotationPresent(Shard)) {
+                        def prop = domainClass.clazz.getAnnotation(Shard)
                         _indexDataSource.set(prop.indexDataSourceName())
                         return (prop.indexDataSourceName())
                     }
@@ -92,4 +101,5 @@ class CurrentShard {
 
         throw new Exception("Error no domain class registered as a Shard lookup class!")
     }
+
 }
